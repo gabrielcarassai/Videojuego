efe4b82f65cde79e5e32a55028f935f957610268
Gabriel <gabrielcarassait@gmail.com>
2024-12-25 22:33:14 -0500

    Commit inicial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="shmup",
+    version="0.0.1",
+    packages=["shmup"],
+    install_requires=["pygame"],
+    entry_points={
+        "console_scripts": [
+            "shmup = shmup.__main__:main"
+        ]
+    },
+)
